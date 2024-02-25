33c18c2534ca9196d0d313b709e7403d875a0af3
PhamDuong113 <129020048+PhamDuong113@users.noreply.github.com>
2024-02-25 10:30:38 +0700

    create bot

---
+++ Bot/bot.js
@@ -0,0 +1,91 @@
+const { Telegraf } = require('telegraf');
+const axios = require('axios');
+
+const {
+	signUpHandle,
+} = require('./controllerBot/access.controllerBot');
+// const { LocalStorage } = require('node-localstorage');
+
+// Tạo một kho lưu trữ local với đường dẫn tạm thời
+// const localStorage = new LocalStorage('./scratch');
+axios.defaults.headers = {
+	'Cache-Control': 'no-cache',
+	Pragma: 'no-cache',
+	Expires: '0',
+};
+
+// bot.telegram.setWebhook(`https://bot-app-english.vercel.app/webhook/${TOKEN}`)
+// bot.telegram.setWebhook(
+// 	`https://api.telegram.org/bot6893164702:AAEPdDlqfEy20Np_goXO7R-9cqAgfelPys0/setWebHook?url=https://bot-app-english.vercel.app`
+// );
+
+const web_link = 'https://bot-app-english.vercel.app/';
+
+const tutorialMessage = `
+	Đây là danh sách các câu lệnh
+	/start - Hiên thị các danh sách sử dụng bot
+	/signup - đăng ký sử dụng app
+`;
+
+// bot.use((ctx, next) => {
+// 	console.log(ctx);
+// 	next();
+// });
+
+const setupBot = () => {
+	const TOKEN = '6893164702:AAEPdDlqfEy20Np_goXO7R-9cqAgfelPys0';
+	const bot = new Telegraf(TOKEN);
+	bot.start((ctx) => {
+		// Gửi tin nhắn và thiết lập nút web_app
+		ctx.reply(`${tutorialMessage}`, {
+			reply_markup: {
+				keyboard: [[{ text: 'web app', web_app: { url: web_link } }]],
+			},
+		});
+
+		// Lưu idTelegram vào localStorage
+		// localStorage.setItem('idTelegram', ctx.from.id.toString());
+	});
+
+	bot.use((ctx, next) => {
+        console.log('Received a message:', ctx.message);
+        return next();
+    });
+
+	//access bot
+	signUpHandle(bot);
+
+	bot.hears('hi', (ctx) => {
+		ctx.reply(`hi ${ctx.from.first_name}! how are you today?`);
+	});
+
+	// bot.on('text', (ctx, next) => {
+	// 	console.log('ctx tẽt:', ctx.update.message.text);
+	// 	ctx.reply('tôi đã nhận được feedback của bạn');
+	// 	next(ctx);
+	// });
+
+	bot.command('test', (ctx) => {
+		ctx.telegram.sendMessage(ctx.chat.id, 'duong dep trai');
+		console.log('chat id:', ctx.chat.id, ctx.from);
+	});
+
+	bot.command('echo', (ctx) => {
+		let input = ctx.message.text;
+		let inputArr = input.split(' ');
+		let message = '';
+		if (inputArr.length === 1) {
+			message = 'không có nội dung';
+		} else {
+			inputArr.shift();
+			console.log({ inputArr });
+			message = inputArr.join(' ');
+		}
+		ctx.reply(message);
+	});
+
+	// bot.launch();
+
+	return bot;
+};
+module.exports = { setupBot };
---
+++ Bot/controllerBot/access.controllerBot.js
@@ -0,0 +1,171 @@
+
+const axios = require('axios');
+const { Tutorial } = require('../CONTANT/tutorial');
+const registrationInfo = {
+	name: '',
+	email: '',
+	password: '',
+};
+const web_link = 'https://bot-app-english.vercel.app/';
+const signUpHandle = (bot) => {
+	bot.command('signup', (ctx) => {
+		// ctx.telegram.sendMessage(ctx.chat.id, Tutorial.signUp);
+		ctx.reply(`${Tutorial.signUp}`, {
+			reply_markup: {
+				keyboard: [[{ text: 'Đăng ký', web_app: { url: `${web_link}/signup` } }]],
+			},
+		})
+	});
+
+
+	// DIỀN THÔNG TIN ĐĂNG KÝ
+	bot.command('name', (ctx) => {kjtr5   
+		let input = ctx.message.text;
+		let inputArr = input.split(' ');
+		let message = '';
+		if (inputArr.length === 1) {
+			message =
+				'Bạn không đặt tên nên mik sẽ lấy tên trên telegram của bạn:';
+			ctx.reply(`${message} ${ctx.from.first_name}`);
+			registrationInfo.name = ctx.from.first_name;
+
+			return;
+		} else {
+			inputArr.shift();
+			console.log({ inputArr });
+			message = inputArr.join(' ');
+			registrationInfo.name = message.toString();
+		}
+		ctx.reply(`Mình đã nhận được thông tin email của bạn : ${message}`);
+	});
+	bot.command('email', (ctx) => {
+		let input = ctx.message.text;
+		let inputArr = input.split(' ');
+		let message = '';
+		if (inputArr.length === 1) {
+			message = 'Bạn cần nhập email của mình';
+			ctx.reply(`${message}`);
+			return;
+		} else {
+			inputArr.shift();
+			console.log({ inputArr });
+			message = inputArr.join(' ');
+			registrationInfo.email = message.toString();
+		}
+		ctx.reply(`Mình đã nhận được thông tin email của bạn : ${message}`);
+	});
+	bot.command('password', (ctx) => {
+		let input = ctx.message.text;
+		let inputArr = input.split(' ');
+		let message = '';
+		if (inputArr.length === 1) {
+			message = 'Bạn cần nhập email của mình';
+			ctx.reply(`${message}`);
+			return;
+		} else {
+			inputArr.shift();
+			console.log({ inputArr });
+			message = inputArr.join(' ');
+			registrationInfo.password = message.toString();
+		}
+		ctx.reply(
+			`Mình đã nhận được thông tin password của bạn : ${message}`
+		);
+	});
+
+	bot.command('submit', async (ctx) => {
+		if (
+			registrationInfo.email === '' ||
+			registrationInfo.password === ''
+		) {
+			ctx.reply(
+				'pikachu thấy bạn còn thiếu thông tin:)). Hãy kiểm tra lại email và password'
+			);
+			return;
+		}
+		ctx.reply(`Thông tin đăng ký của bạn là:
+		name:  ${
+			registrationInfo.name
+				? registrationInfo.name
+				: (registrationInfo.name = ctx.from.first_name)
+		}
+		email: ${registrationInfo.email}
+		password: ${registrationInfo.password}
+	
+	`);
+	ctx.reply('Quá trình đăng ký đăng diễn ra ...')
+
+		const headers = {
+			'x-api-key':
+				'4379e3b406e606110a01e8fbe364120fdc58be39a9f30431476dd53ad14b20fe66f52423a3e4546dfa272f4c389822299709414bb44b6b3ffce7f04292be2556',
+		};
+		const signUp = await axios
+			.post(
+				`https://bot-app-english.vercel.app/signup`,
+				{
+					name: registrationInfo.name,
+					email: registrationInfo.email,
+					password: registrationInfo.password,
+				},
+				{
+					headers,
+				}
+			)
+			.then((res) => {
+				return res.data;
+			})
+			.catch((error) => {
+				console.log(`ApiService: ${error}`);
+				if (error.response) {
+					// Request made and server responded
+					return error.response.data;
+				} else if (error.request) {
+					// The request was made but no response was received
+					console.log(error.request);
+					return error;
+				} else {
+					// Something happened in setting up the request that triggered an Error
+					console.log('Error', error);
+					return error;
+				}
+			});
+		
+	
+		if (signUp.status == '200') {
+			ctx.reply(`Bạn đã đăng ký tài khoản thành công!
+					Đây là thông tin tài khoản của bạn:
+						name:  ${
+							registrationInfo.name
+								? registrationInfo.name
+								: (registrationInfo.name = ctx.from.first_name)
+						}
+						email: ${registrationInfo.email}
+						password: ${registrationInfo.password}
+
+					TÔI RẤT VUI KHI ĐƯỢC ĐỒNG HÀNH VỚI BẠN:)))
+					
+
+			
+					`)
+		} else {
+			ctx.reply(`${signUp.message}`)
+		}
+
+	});
+
+	bot.command('check_Signup', (ctx) => {
+		ctx.reply(`Kiểm tra lại thông tin đăng ký của bạn là:
+					name:  ${
+						registrationInfo.name
+							? registrationInfo.name
+							: (registrationInfo.name = ctx.from.first_name)
+					}
+					email: ${registrationInfo.email}
+					password: ${registrationInfo.password}
+				
+				`);
+		
+	});
+};
+
+module.exports = { signUpHandle };
