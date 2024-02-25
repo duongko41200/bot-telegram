a6125f428ada6f41de29194549264c9fde48937c
PhamDuong113 <129020048+PhamDuong113@users.noreply.github.com>
2024-02-25 11:18:44 +0700

    update bot

--- server.js
+++ server.js
@@ -1,25 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { setupBot } = require('./Bot/bot');
+const { Telegraf } = require('telegraf');
+
+const TOKEN = '6893164702:AAEPdDlqfEy20Np_goXO7R-9cqAgfelPys0';
+const bot = new Telegraf(TOKEN);
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Thiết lập Bot
-const bot = setupBot();
+// const web_link = 'https://bot-telegram-swart-seven.vercel.app/';
 
-// Middleware để xử lý JSON và xử lý Webhook từ Telegram
+// const tutorialMessage = `
+// 	Đây là danh sách các câu lệnh
+// 	/start - Hiển thị các danh sách sử dụng bot
+// 	/signup - Đăng ký sử dụng app
+// `;
+
+// bot.start((ctx) => {
+// 	// Gửi tin nhắn và thiết lập nút web_app
+// 	ctx.reply(`${tutorialMessage}`, {
+// 		reply_markup: {
+// 			keyboard: [[{ text: 'web app', web_app: { url: web_link } }]],
+// 		},
+// 	});
+// });
+
 app.use(bodyParser.json());
 
-// Xử lý Webhook từ Telegram
 app.post('/webhook', (req, res) => {
-    bot.handleUpdate(req.body, res);
+	bot.handleUpdate(req.body, res);
 });
 
-// Lắng nghe trên cổng đã chọn
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-    const webhookUrl = 'https://bot-app-english.vercel.app/';
-    bot.telegram.setWebhook(`${webhookUrl}/webhook`);
-    console.log(`Webhook has been set up at: ${webhookUrl}/webhook`);
+	console.log(`Server is running on port ${port}`);
+	const webhookUrl = 'https://bot-telegram-swart-seven.vercel.app';
+	bot.telegram.setWebhook(`${webhookUrl}/webhook`);
+	console.log(`Webhook has been set up at: ${webhookUrl}/webhook`);
 });
