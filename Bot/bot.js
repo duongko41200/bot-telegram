7d6dda47e54bb233ef7fece58ff2a824529a8893
PhamDuong113 <129020048+PhamDuong113@users.noreply.github.com>
2024-05-23 21:54:51 +0700

    fix;data

--- Bot/bot.js
+++ Bot/bot.js
@@ -16,7 +16,6 @@ axios.defaults.headers = {
 	Expires: '0',
 };
 
-
 // bot.telegram.setWebhook(`https://bot-app-english.vercel.app/webhook/${TOKEN}`)
 // bot.telegram.setWebhook(
 // 	`https://api.telegram.org/bot6893164702:AAEPdDlqfEy20Np_goXO7R-9cqAgfelPys0/setWebHook?url=https://bot-app-english.vercel.app`
@@ -30,14 +29,10 @@ const tutorialMessage = `
 	/signup - đăng ký sử dụng app
 `;
 
-
-
 // bot.use((ctx, next) => {
 // 	console.log(ctx);
 // 	next();
 // });
-
-
 
 const setupBot = () => {
 	const TOKEN = '6893164702:AAEPdDlqfEy20Np_goXO7R-9cqAgfelPys0';
@@ -55,9 +50,9 @@ const setupBot = () => {
 	});
 
 	bot.use((ctx, next) => {
-        console.log('Received a message:', ctx.message);
-        return next();
-    });
+		console.log('Received a message:', ctx.message);
+		return next();
+	});
 
 	//access bot
 	signUpHandle(bot);
@@ -66,75 +61,76 @@ const setupBot = () => {
 		ctx.reply(`hi ${ctx.from.first_name}! how are you today?`);
 	});
 
-
-
 	bot.command('test', (ctx) => {
 		ctx.telegram.sendMessage(ctx.chat.id, 'duong dep trai');
 		console.log('chat id:', ctx.chat.id, ctx.from);
 	});
 
-
-
-
-
 	const userNotifications = {};
 
-	bot.command("set", (ctx) => {
+	bot.command('set', (ctx) => {
 		const message = ctx.message.text;
-	  
+
 		// Extract the time and message using a regular expression
 		const match = message.match(/^\/set\s+([\d:]+)\s+(.+)/);
-	  
+
 		// Check if the time and message are provided
-		if (match) {
-		  const time = match[1];
-		  const notificationMessage = match[2];
-	  
-		  // Save the notification for the user
-		  const userId = ctx.message.from.id;
-	  
-		  // Check if the user already has notifications
-		  if (!userNotifications[userId]) {
-			userNotifications[userId] = [];
-		  }
-	  
-		  // Add the new notification to the array
-			userNotifications[userId].push({ time, message: notificationMessage });
-			console.log("userNotifications: ",userNotifications)
-	  
-		  ctx.reply(`✅ Notification set for ${time} - ${notificationMessage}`);
-	  
-		  // Schedule the notification
-		  scheduleNotification(userId, time, notificationMessage);
-		} else {
-		  ctx.reply(
-			  "⚠️ Please provide both time and notification message in the correct format. " +
-			  "\n\nExample format: `/set 12:30 Cook some dinner`"
-		  );
+		try {
+			if (match) {
+				const time = match[1];
+				const notificationMessage = match[2];
+
+				// Save the notification for the user
+				const userId = ctx.message.from.id;
+
+				// Check if the user already has notifications
+				if (!userNotifications[userId]) {
+					userNotifications[userId] = [];
+				}
+
+				// Add the new notification to the array
+				userNotifications[userId].push({
+					time,
+					message: notificationMessage,
+				});
+				console.log('userNotifications: ', userNotifications);
+
+				ctx.reply(
+					`✅ Notification set for ${time} - ${notificationMessage}`
+				);
+
+				// Schedule the notification
+				scheduleNotification(userId, time, notificationMessage);
+			} else {
+				ctx.reply(
+					'⚠️ Please provide both time and notification message in the correct format. ' +
+						'\n\nExample format: `/set 12:30 Cook some dinner`'
+				);
+			}
+		} catch (error) {
+			console.log({ error });
 		}
 	});
 
-	function scheduleNotification(
-		userId,
-		time,
-		message
-	) {
-		const [hours, minutes] = time.split(':');
-	
-		// Schedule the job
+	function scheduleNotification(userId, time, message) {
+		try {
+			const [hours, minutes] = time.split(':');
 
-		console.log("data vào đây")
-	
-		userNotifications[userId].job = schedule.scheduleJob(
-			{ hour: parseInt(hours), minute: parseInt(minutes) },
-			() => {
-				// Send the notification to the user
-				bot.telegram.sendMessage(userId, `⏰ Reminder: ${message}`);
-			}
-		);
+			// Schedule the job
+
+			console.log('data vào đây');
+
+			userNotifications[userId].job = schedule.scheduleJob(
+				{ hour: parseInt(hours), minute: parseInt(minutes) },
+				() => {
+					// Send the notification to the user
+					bot.telegram.sendMessage(userId, `⏰ Reminder: ${message}`);
+				}
+			);
+		} catch (error) {
+			console.log({ error });
+		}
 	}
-
-
 
 	bot.command('echo', (ctx) => {
 		let input = ctx.message.text;
@@ -154,5 +150,5 @@ const setupBot = () => {
 
 	return bot;
 };
-setupBot()
+// setupBot()
 module.exports = { setupBot };
